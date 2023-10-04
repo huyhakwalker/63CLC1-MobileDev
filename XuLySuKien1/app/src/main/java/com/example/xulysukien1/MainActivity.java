18a989f326bc32dfd7c1d43830db44a69690ded0
Nguyễn Quốc Huy <huyhakwalker@gmail.com>
2023-10-04 09:31:04 +0700

    Update MainActivity.java

--- XuLySuKien1/app/src/main/java/com/example/xulysukien1/MainActivity.java
+++ XuLySuKien1/app/src/main/java/com/example/xulysukien1/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWiget();
+    }
+    void getWiget(){
         user = findViewById(R.id.user);
         pass = findViewById(R.id.pass);
     }
