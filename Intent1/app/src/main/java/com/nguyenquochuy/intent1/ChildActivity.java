076fa9f11ba7c2f3dae1ec31886fdcd59f8379da
Nguyễn Quốc Huy <huyhakwalker@gmail.com>
2023-10-24 19:38:07 +0700

    Renew

--- Intent1/app/src/main/java/com/nguyenquochuy/intent1/ChildActivity.java
+++ Intent1/app/src/main/java/com/nguyenquochuy/intent1/ChildActivity.java
@@ -8,13 +8,18 @@ import android.view.View;
 import android.widget.Button;
 
 public class ChildActivity extends AppCompatActivity {
-
     Button btnback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+        get();
+        back();
+    }
+    public void get(){
         btnback = findViewById(R.id.btnback);
+    }
+    public void back(){
         btnback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
