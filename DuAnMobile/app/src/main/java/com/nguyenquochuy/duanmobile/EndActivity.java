2acad28c3e333f82de0a3c5649c17deae5dda7d0
Nguyễn Quốc Huy <huyhakwalker@gmail.com>
2024-01-04 10:35:11 +0700

    update: bản cuối + file báo cáo powerpoint

--- DuAnMobile/app/src/main/java/com/nguyenquochuy/duanmobile/EndActivity.java
+++ DuAnMobile/app/src/main/java/com/nguyenquochuy/duanmobile/EndActivity.java
@@ -21,6 +21,7 @@ public class EndActivity extends AppCompatActivity {
         setContentView(R.layout.activity_end);
         get();
         MainActivity m = new MainActivity();
+        //MainActivity.getFinalScore();
         switch (m.getFinalScore()) {
             case 1:
                 tv_score_end.setText("Điểm số cuối cùng của bạn là: 200.000");
